bc1544edbbf4bfacdb6d4a009855311698801f93
Joshua Sevy <joshuasevy@outlook.com>
2020-09-02 20:54:51 -0600

    Add whitewater data set

---
+++ whitewater.js
@@ -0,0 +1,101 @@
+export const whitewater = [
+  {
+    name: "Rico Falls",
+    river: "Upper Dolores",
+    cfs: 250,
+    class: "V",
+    miles: 2,
+    put_in: "lizard head pass",
+    take_out: "Rico"
+  },
+  {
+    name: "Yule Creek",
+    river: "Crystal Drainage",
+    cfs: 800,
+    class: "V",
+    miles: 5,
+    put_in: ".1 mile downstream from bible camp",
+    take_out: "Beaver lake in Marble"
+  },
+  {
+    name: "Big South",
+    river: "Cache La Poudre",
+    cfs: 300,
+    class: "V+ *3",
+    miles: 12,
+    put_in: "Long Draw Res outflow",
+    take_out: "Big South Trailhead"
+  },
+  {
+    name: "Clear Creek Ark",
+    river: "Arkansas River",
+    cfs: 350,
+    class: "V-",
+    miles: 2.5,
+    put_in: "Missouri Creek Trailhead",
+    take_out: "two miles past Clear Creek Ranch, at a dirt road"
+  },
+  {
+    name: "Hermosa Creek",
+    river: "Animas River",
+    cfs: 1000,
+    class: "IV-",
+    miles: 6.5,
+    put_in: "Hermosa Creek Trail",
+    take_out: "FR 576"
+  },
+  {
+    name: "Pandoras Box",
+    river: "Animas River",
+    cfs: 600,
+    class: "V++",
+    miles: 2.5,
+    put_in: "Rockwood Depot illegal hike in point that is GUIDE MANDATORY",
+    take_out: "Baker's Bridge"
+  },
+  {
+    name: "Grand Canyon",
+    river: "Colorado River",
+    cfs: 6000,
+    class: "IV",
+    miles: 250,
+    put_in: "Lees Ferry",
+    take_out: "Lake Mead"
+  },
+  {
+    name: "Stix & Stones",
+    river: "Clarks Fork",
+    cfs: 300,
+    class: "V",
+    miles: 4.7,
+    put_in: "Cheif Joseph Trialhead",
+    take_out: "Fox Creek Campground"
+  },
+  {
+    name: "Shoshone",
+    river: "Clarks Fork",
+    cfs: 500,
+    class: "IV-",
+    miles: 3.6,
+    put_in: "HWY 120 culvert or Red Canyon river access",
+    take_out: "Red Canyon river access or HWY 14 bridge"
+  },
+  {
+    name: "Shoshone",
+    river: "Clarks Fork",
+    cfs: 500,
+    class: "IV-",
+    miles: 3.6,
+    put_in: "HWY 120 culvert or Red Canyon river access",
+    take_out: "Red Canyon river access or HWY 14 bridge"
+  },
+  {
+    name: "Taylor Canyon",
+    river: "Taylor River",
+    cfs: 400,
+    class: "III",
+    miles: 5,
+    put_in: "New Generation or South Bank Launch",
+    take_out: "South Bank Launch or Almont Post Office"
+  }
+]
